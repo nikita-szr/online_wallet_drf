1a2f5ebbe819432eadc387d7886f3fd03333feca
nikitt <nikittaosipov@yandex.ru>
2025-03-28 23:09:25 +0400

    transaction model

--- wallets/models.py
+++ wallets/models.py
@@ -15,3 +15,23 @@ class Wallet(models.Model):
     def __str__(self):
         return f'Кошелек {self.uuid} - Баланс: {self.balance} - Последняя операция: {self.last_update_at}'
 
+
+class Transaction(models.Model):
+    OPERATION_CHOICES = [
+        ('DEPOSIT', 'Deposit'),
+        ('WITHDRAW', 'Withdraw')
+    ]
+
+    STATUS_CHOICES = [
+        ("SUCCESS", "Success"),
+        ("FAILED", "Failed"),
+    ]
+
+    wallet = models.ForeignKey(Wallet, on_delete=models.CASCADE)
+    operation_type = models.CharField(max_length=10, choices=OPERATION_CHOICES)
+    amount = models.DecimalField(max_digits=20, decimal_places=2)
+    created_at = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default="SUCCESS")
+
+    def __str__(self):
+        return f"{self.operation_type} {self.amount} ({self.status})"
