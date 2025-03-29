5820c45b6da9889018c449f8c0b6554c8671305e
nikitt <nikittaosipov@yandex.ru>
2025-03-29 16:25:51 +0400

    urls

---
+++ wallets/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from views import WalletListCreateView, WalletDetailView, WalletOperationView
+
+
+urlpatterns = [
+    path('api/v1/wallets/', WalletListCreateView.as_view(), name='wallet_list_create'),
+    path('api/v1/wallets/<uuid:wallet_uuid>/', WalletDetailView.as_view(), name='wallet_detail'),
+    path('api/v1/wallets/<uuid:wallet_uuid>/operation/', WalletOperationView.as_view(), name='wallet_operation'),
+]
+
